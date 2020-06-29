707bd9b3e2be60a80025d4188056be56885f8a92
Maciej Korsan <maciejkorsan@gmail.com>
2020-06-30 00:19:31 +0200

    acceleration transform

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ if (typeof DeviceMotionEvent.requestPermission === "function") {
     DeviceMotionEvent.requestPermission().then((response) => {
       if (response == "granted") {
         window.addEventListener("devicemotion", (e) => {
-          console.log(e);
+          
+  root.style.setProperty('--change-x', `${e.acceleration.x*100}px`);
+  root.style.setProperty('--change-y', `${e.acceleration.y*100}px`);
         });
       }
     });
   });
 } else {
-  // non iOS 13+
+  alert('non 13')
 }
